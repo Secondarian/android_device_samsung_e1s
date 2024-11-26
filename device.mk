2c20b6c14dcee0f104e94a0221c721525e984e5b
Secondarian <robertanitei@tutanota.com>
2024-11-26 19:47:07 +0200

    e1s: Basic vendor setup

--- device.mk
+++ device.mk
@@ -31,6 +31,7 @@ PRODUCT_PACKAGES += \
 
 # Init
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/init/fstab.s5e9945:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.s5e9945 \
     $(LOCAL_PATH)/configs/init/fstab.s5e9945:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/fstab.s5e9945 \
     $(LOCAL_PATH)/configs/init/init.recovery.s5e9945.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.s5e9945.rc
 
@@ -42,6 +43,10 @@ PRODUCT_PACKAGES += \
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_PACKAGES += vndservicemanager
+
 
 # Proprietary files
 $(call inherit-product, vendor/samsung/e1s/e1s-vendor.mk)
