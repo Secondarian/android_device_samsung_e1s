bf1d56aa5e16bf6d252fc10496a91e01efb285ab
Secondarian <robertanitei@tutanota.com>
2024-11-30 20:05:49 +0200

    e1s: libshims: Adapt libshim_ui to lockAsync

--- libshims/libshim_ui/GraphicBufferMapper.cpp
+++ libshims/libshim_ui/GraphicBufferMapper.cpp
@@ -13,10 +13,11 @@ using android::Rect;
 using android::status_t;
 
 extern "C" {
-status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(
-        void* thisptr, buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr,
-        int32_t* /*outBytesPerPixel*/, int32_t* /*outBytesPerStride*/) {
+status_t _ZN7android19GraphicBufferMapper9lockAsyncEPK13native_handlemmRKNS_4RectEPPviPiS9_(
+        void* thisptr, buffer_handle_t handle, uint64_t producerUsage, uint64_t consumerUsage,
+        const Rect& bounds, void** vaddr, int fenceFd, int32_t* /*outBytesPerPixel*/,
+        int32_t* /*outBytesPerStride*/) {
     auto* gpm = static_cast<android::GraphicBufferMapper*>(thisptr);
-    return gpm->lock(handle, usage, bounds, vaddr);
+    return gpm->lockAsync(handle, producerUsage, consumerUsage, bounds, vaddr, fenceFd);
 }
 }
